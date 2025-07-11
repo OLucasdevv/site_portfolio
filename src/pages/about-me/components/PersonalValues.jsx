234f388681ea113a50dd91616689293f00687fea
Lucas <lucastiusammm@gmail.com>
2025-07-11 14:46:40 -0300

    buttonsBugRes

--- src/pages/about-me/components/PersonalValues.jsx
+++ src/pages/about-me/components/PersonalValues.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Icon from '../../../components/AppIcon';
+import { Link } from 'react-router-dom';
 
 const PersonalValues = () => {
   const [currentLanguage, setCurrentLanguage] = useState('en');
